e1c1473d6c8ecd0e416cb19b068a426677d0d714
matheusfconde <matheusf.conde@gmail.com>
2025-11-04 19:26:40 -0300

    feat: Criação interface repositório banco

--- src/Application/DTOs/ProductoDto.cs
+++ src/Application/DTOs/ProductoDto.cs
@@ -1,11 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Application.DTOs;
 
-namespace Application.DTOs;
-
-internal class ProductoDto
-{
-}
+public record ProductoDto(Guid id, string Name, string Category, decimal UnitCost, DateTime CreatedAt);
--- src/Application/Interfaces/IProductRepository.cs
+++ src/Application/Interfaces/IProductRepository.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Entities;
 
 namespace Application.Interfaces;
 
-internal interface IProductRepository
+public interface IProductRepository
 {
+    Task<Product> AddAsync(Product product, CancellationToken cancellationToken = default);
+    Task UpdateAsync(Product product, CancellationToken cancellationToken = default);
+    Task<Product?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Product>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
 }
