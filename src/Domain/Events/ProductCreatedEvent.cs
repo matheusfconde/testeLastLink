ac3c78ada1205f0ff5c7aaea978600c9b7199f3d
matheusfconde <matheusf.conde@gmail.com>
2025-11-04 19:20:03 -0300

    feat: initial commit

---
+++ src/Domain/Events/ProductCreatedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Events;
+
+public class ProductCreatedEvent
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; } = null!;
+    public string Category { get; init; } = null!;
+    public decimal UnitCost { get; init; }
+    public DateTime CreatedAt { get; init; }
+}
